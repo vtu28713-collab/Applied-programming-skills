a8244d73cfbf9b706127e69abb3e35164056857c
vtu28713-collab <vtu28713@veltech.edu.in>
2026-09-01 14:42:38 +0530

    Time: 109 ms (64.43%), Space: 17.2 MB (11.92%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.py
@@ -0,0 +1,14 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.stack = []
+
+    def next(self, price):
+        span = 1
+
+        while self.stack and self.stack[-1][0] <= price:
+            span += self.stack.pop()[1]
+
+        self.stack.append((price, span))
+
+        return span
