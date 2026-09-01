6dc8149fdd94cc1dd2e73f7ed95fbe92ad23ce17
vtu28713-collab <vtu28713@veltech.edu.in>
2026-09-01 14:40:15 +0530

    Time: 3 ms (52.92%), Space: 12.7 MB (24.1%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,20 +1,17 @@
-class Solution(object):
+class Solution:
     def nextGreaterElement(self, nums1, nums2):
         stack = []
-        next_greater = {}
+        greater = {}
 
         for num in nums2:
-            # If current number is greater than stack top,
-            # it is the next greater element
             while stack and num > stack[-1]:
                 smaller = stack.pop()
-                next_greater[smaller] = num
+                greater[smaller] = num
 
             stack.append(num)
 
-        # Elements with no greater element
+        # Remaining elements have no greater element
         while stack:
-            next_greater[stack.pop()] = -1
+            greater[stack.pop()] = -1
 
-        # Build answer for nums1
-        return [next_greater[num] for num in nums1]
+        return [greater[num] for num in nums1]
