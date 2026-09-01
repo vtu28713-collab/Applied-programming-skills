9b22f9d21bb9c94a90d9a02c7ac864fad1cd36d5
vtu28713-collab <vtu28713@veltech.edu.in>
2026-09-01 14:41:08 +0530

    Time: 124 ms (99.41%), Space: 26 MB (83.89%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,14 @@
+class Solution:
+    def dailyTemperatures(self, temperatures):
+        answer = [0] * len(temperatures)
+        stack = []
+
+        for i, temp in enumerate(temperatures):
+
+            while stack and temp > temperatures[stack[-1]]:
+                prev = stack.pop()
+                answer[prev] = i - prev
+
+            stack.append(i)
+
+        return answer
